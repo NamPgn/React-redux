a27127fb3a1a6757b14132ba1e6ff3407bb677de
nampg <nampham18082003@gmail.com>
2024-10-10 18:23:34 +0700

    update layout episode detail movie

--- src/components/Seri/SeriDetail.tsx
+++ src/components/Seri/SeriDetail.tsx
@@ -6,34 +6,35 @@ import MVCol from "../MV/Grid/Col";
 
 const SeriDetailProducts = memo(({ seriProduct }: any) => {
   return (
-    <MVRow gutter={14} items="center">
-      {seriProduct?.category?.products?.map((item: any, index: any) =>
-        item.isApproved == true ? (
-          <MVCol
-            xl={3}
-            lg={3}
-            md={4}
-            sm={4}
-            xs={6}
-            key={index}
-            className="mt-2"
+    <MVRow gutter={14} items="center" className="overflow-x-auto custom-scrollbar">
+    {seriProduct?.category?.products?.map((item: any, index: any) =>
+      item.isApproved == true ? (
+        <MVCol
+          xl={3}
+          lg={3}
+          md={4}
+          sm={4}
+          xs={6}
+          key={index}
+          className="mt-2"
+        >
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isActive ? "activeSeri" : ""
+            }
+            to={"/d/" + item.slug}
           >
-            <NavLink
-              className={({ isActive, isPending }) =>
-                isActive ? "activeSeri" : ""
-              }
-              to={"/d/" + item.slug}
-            >
-              <BtnStyled className={item.seri && "w-full"}>
-                {item.seri && "Tập " + item.seri}
-              </BtnStyled>
-            </NavLink>
-          </MVCol>
-        ) : (
-          ""
-        )
-      )}
-    </MVRow>
+            <BtnStyled className={item.seri && "w-full"}>
+              {item.seri && "Tập " + item.seri}
+            </BtnStyled>
+          </NavLink>
+        </MVCol>
+      ) : (
+        ""
+      )
+    )}
+  </MVRow>
+  
   );
 });
 
--- src/components/Week/index.tsx
+++ src/components/Week/index.tsx
@@ -60,7 +60,7 @@ export default function WeekComponent() {
         {isLoading ? (
           <Loading />
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
             {categorys?.content?.map((itemsCategory) => (
               <div className="px-2" key={itemsCategory._id}>
                 <CategoryContents
